4e13135432b29ed11e4f91492b01eea6bbd70b4b
Hernán Chumpitaz <hernan.chumpitaz@vallegrande.edu.pe>
2024-10-21 00:24:51 -0500

    update

--- src/main/java/pe/edu/vallegrande/config/WebClientConfig.java
+++ src/main/java/pe/edu/vallegrande/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package pe.edu.vallegrande.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+   @Value("${RAPIDAPI_KEY}")
+   private String rapidApiKey;
+
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder.baseUrl("https://chatgpt-api8.p.rapidapi.com")
-                .defaultHeader("x-rapidapi-key", "5258e2b9edmsh19d0d7a4a0cdc57p18c749jsn9a0d4349d129")
+                .defaultHeader("x-rapidapi-key", "rapidApiKey" )
                 .defaultHeader("x-rapidapi-host", "chatgpt-api8.p.rapidapi.com")
                 .defaultHeader("Content-Type", "application/json; charset=UTF-8")
                 .build();
--- src/main/java/pe/edu/vallegrande/controller/ChatGptController.java
+++ src/main/java/pe/edu/vallegrande/controller/ChatGptController.java
@@ -23,4 +23,3 @@ public class ChatGptController {
         return chatGptService.getAllChatGptModels();
     }
 }
-
