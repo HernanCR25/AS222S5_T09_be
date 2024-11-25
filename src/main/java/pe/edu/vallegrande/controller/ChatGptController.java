6f9b8fd99138a846dddb5f9cad7694ff40abaace
Hernán Chumpitaz <111783000+HernanCR25@users.noreply.github.com>
2024-11-25 15:29:24 -0500

    Update ChatGptController.java

--- src/main/java/pe/edu/vallegrande/controller/ChatGptController.java
+++ src/main/java/pe/edu/vallegrande/controller/ChatGptController.java
@@ -7,19 +7,55 @@ import pe.edu.vallegrande.service.ChatGptService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ChatGptController {
-    
+
     private final ChatGptService chatGptService;
 
     @PostMapping("/ask")
     public Mono<String> askChatGpt(@RequestBody String requestBody) {
         return chatGptService.sendRequest(requestBody);
     }
+
     @GetMapping
     public Flux<ChatGptModel> getAllChatGptModels() {
         return chatGptService.getAllChatGptModels();
+    }
+    @GetMapping("/{id}")
+public Mono<ChatGptModel> getChatGptModelById(@PathVariable Long id) {
+    return chatGptService.getChatGptModelById(id);
+}
+
+    @GetMapping("/active")
+    public Flux<ChatGptModel> getActiveChatGptModels() {
+        return chatGptService.getActiveChatGptModels();
+    }
+
+    @GetMapping("/inactive")
+    public Flux<ChatGptModel> getInactiveChatGptModels() {
+        return chatGptService.getInactiveChatGptModels();
+    }
+
+    @PutMapping("/update/{id}")
+    public Mono<ChatGptModel> updateChatGptModel(@PathVariable Long id, @RequestBody String newConsulta) {
+        return chatGptService.updateChatGptModel(id, newConsulta);
+    }    
+
+    @PatchMapping("/deactivate/{id}")
+    public Mono<ChatGptModel> deactivateChatGptModel(@PathVariable Long id) {
+        return chatGptService.deactivateChatGptModel(id);
+    }
+
+    @PatchMapping("/activate/{id}")
+    public Mono<ChatGptModel> activateChatGptModel(@PathVariable Long id) {
+        return chatGptService.activateChatGptModel(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Mono<Void> deleteChatGptModel(@PathVariable Long id) {
+        return chatGptService.deleteChatGptModel(id);
     }
 }
